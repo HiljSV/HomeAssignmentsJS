d1dd5faf70499d05147c41d1c9d262f014a2a802
Serhii <hiljsv@gmail.com>
2025-08-28 18:26:48 +0300

    Lesson 6 JS 2025

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-// Строгий режим
-"use strict"
-// --------------------------------------------------
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 23;
+a = 12 + 12;
+console.log('a = ${a}');
+document.write('ok!');
+const A = 23;
+A = 27;
+console.log(A);
+//# sourceMappingURL=script.js.map
---
+++ src/script.ts
@@ -0,0 +1,4 @@
+let a: number = 23;
+a = 12+12;
+console.log('a = ${a}');
+document.write('ok!')
